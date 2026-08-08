58e99020708e3a8c87674f559ec312a0788a8d5d
joyakubu <joelyakubu269@gmail.com>
2026-08-08 14:43:31 +0100

    Add hypotenuse calculator

--- hypCal.py
+++ hypCal.py
@@ -2,4 +2,12 @@ import math
 def hyp(base, height):
        val = base ** 2 + height ** 2
        return math.sqrt(val)
-print(hyp(3,4))
+val1= input("enter the base ")
+val2 = input("enter the height ")
+try:
+       num1= int(val1)
+       num2 = int(val2)
+except ValueError:
+       print("Invalid number")
+
+print(hyp(num1,num2))
