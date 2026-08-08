ce364306df3b982d1ca9e24b824921c494469c44
joyakubu <joelyakubu269@gmail.com>
2026-08-08 17:23:07 +0100

    experimenting with the datatypes in python list being the first on the list

--- dataTypes.py
+++ dataTypes.py
@@ -1,2 +1,9 @@
 val = [1,2,3,4,5,6,8,9,10]
 print(val[7::-2])
+val2= [1,2,3,4,5]
+for x in val2:
+        print(x)
+        if x == 2:
+            print("the value is 2")
+        else:
+            print("the value is not 2")
