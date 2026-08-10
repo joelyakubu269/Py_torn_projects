72c39c5a42e7c9c353f029db84f9ec7767dc10b8
joyakubu <joelyakubu269@gmail.com>
2026-08-10 23:52:15 +0100

    now on tuples

--- tuples.py
+++ tuples.py
@@ -1,4 +1,10 @@
 x = (1, 2, 3)
 print(x[0]) # indexing test
-print(x[0:2]) slicing without step
+print(x[0:2]) #slicing without step
 print(x[0::2]) # slicing with step
+# you cannot add to a tuple x.append(9)
+#tuples are immutable but that does not mean they cannot change
+y= (1,2,3)
+#y[0].append(9) will give an error
+# tuples can be reassigned
+y= (4,5,6)
