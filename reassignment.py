945eec73b3913b88c3b17b6f76e750762dc3179b
joyakubu <joelyakubu269@gmail.com>
2026-08-10 21:04:52 +0100

    understanding how python datatypes work

--- reassignment.py
+++ reassignment.py
@@ -18,3 +18,9 @@ copy_numbers = numbers.copy()
 copy_numbers.append(40)
 print(numbers)
 print(copy_numbers) # different result becuase what you do the copy does not affect the original
+# deep copy
+number = [[10, 20], [30, 40]]
+copy_number = number.copy()
+number[0][0]= 1
+print(number)
+print(copy_number)
