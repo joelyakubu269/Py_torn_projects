5c54edbfcfedad8d5c956b75cb0b9acf2de6dd0e
joyakubu <joelyakubu269@gmail.com>
2026-08-10 20:49:05 +0100

    understanding how python datatypes work

--- reassignment.py
+++ reassignment.py
@@ -4,3 +4,11 @@ y = x
 x = [20]
 print(x) # prints 20 because its been reassigned
 print(y) # prints 10 becuase the original value has not been changed
+# aliasing
+a= [10,20,30]
+b= a
+a.append(40)
+a[0]= 5
+a.remove(20)
+print(a) # the both print the same value bcoz they still point to the same value
+print(b)
