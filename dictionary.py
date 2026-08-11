3f40573976b8ecd132c0f5424e0aaa230bf1dd1e
joyakubu <joelyakubu269@gmail.com>
2026-08-11 04:18:02 +0100

    now on tuples

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+# in this exercise i study the behaviour of dictionaries when i assign, copy,nest and mutate
+person = {
+    "name": "Joel",
+    "age": 25
+}
+
+other = person
+
+other["age"] = 30
+
+print(person)
+print(other) # print the same value becuase the point to the same value
+person1 = {
+    "name": "Joel",
+    "age": 25
+}
+
+other1 = person.copy()
+
+other1["age"] = 30
+
+print(person1)
+print(other1) # the reuslts are different because they dont point to the same value while other1 is just a copy
