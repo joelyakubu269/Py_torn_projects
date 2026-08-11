b223d460fbb4b0ebc994fe87f75f0d780d463787
joyakubu <joelyakubu269@gmail.com>
2026-08-11 08:01:57 +0100

    now on dictionaries

--- dictionary.py
+++ dictionary.py
@@ -34,4 +34,28 @@ other2 = person2.copy()
 other2["details"]["age"] = 30
 
 print(person2)
-print(other2)
+print(other2) # here modifying the copy changes the original because they both have an inner shared dictionary
+# Mutating vs Replacing
+person3 = {
+    "name": "Joel",
+    "scores": [70, 80, 90]
+}
+
+other3 = person3.copy()
+
+other3["scores"].append(100)
+
+print(person3)
+print(other3) # they both print the same thing becuase the dictionaries are seperate entities but share(point to) the same inner list
+# Reassigning in Dictionary
+person4 = {
+    "name": "Joel",
+    "scores": [70, 80, 90]
+}
+
+other4 = person4.copy()
+
+other4["scores"] = [70, 80, 90, 100]
+
+print(person4)
+print(other4) # the dont print the same result because reassigning creates a new list it does not change the original list
