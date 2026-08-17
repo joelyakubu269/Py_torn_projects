5bfd2f5c8b597d7a191246df0220f8b75e2c905e
joyakubu <joelyakubu269@gmail.com>
2026-08-17 22:44:22 +0100

    python projects

--- gradeManager.py
+++ gradeManager.py
@@ -1,13 +1,13 @@
 def GradeDecider():
     name = input("what is your name ?")
-    score = input("what did you score ?")
+    score = int(input("What did you score? "))
     print(name)
     print(score)
     Grade = ""
-    results = {
-        name: [score,Grade]
-    }
+
     if score > 90:
         Grade = "A"
     elif score > 80 and score < 90 :
         Grade = "B"
+    elif  score > 70 and score < 80:
+        Grade = "C"
