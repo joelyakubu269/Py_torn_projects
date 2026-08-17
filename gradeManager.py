62a7058dbf17839fb04655068115cf45a90dfcc8
joyakubu <joelyakubu269@gmail.com>
2026-08-17 22:47:30 +0100

    python projects

--- gradeManager.py
+++ gradeManager.py
@@ -13,3 +13,8 @@ def GradeDecider():
         Grade = "C"
     elif  score > 60 and score < 70 :
         Grade = "D"
+    elif score > 50 and score < 60 :
+        Grade = "E"
+    else:
+        Grade= "F"
+    results
