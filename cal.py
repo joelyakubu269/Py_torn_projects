8b9e679a5f4ba1759b18b9b1297a9fc348ba3d88
joyakubu <joelyakubu269@gmail.com>
2026-08-18 23:09:53 +0100

    a simple calculator

--- cal.py
+++ cal.py
@@ -1,3 +1,6 @@
 def cali():
         num1 = int(input("pick a number? "))
         opr= int(input("1.(*), 2(+),3(/)"))
+        num2 = int(input("pick a second number? "))
+        print(num1)
+        print("Now select a number representing the operation you want to carry out")
